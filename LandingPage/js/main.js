85b937b5768d2aec89866f293f21183a7379fde1
Matheus Raimundo <61631379+Mathiore@users.noreply.github.com>
2021-11-03 16:16:37 -0300

    Projeto full

---
+++ LandingPage/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('nav').toggleClass('nav-toggle');
+    });
+    $('nav ul li a').click(function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('nav').removeClass('nav-toggle');
+    });
+});
+
